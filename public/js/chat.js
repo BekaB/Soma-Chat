7260e2c4545011fbba7dcb635f04092fa0aa5c4d
Bereket Negash <negash.bereket.se@gmail.com>
2025-01-31 04:21:45 +0400

    event emitting

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,13 @@
-io()
+const socket = io()
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    const message = document.getElementById('message').value
+
+    socket.emit('sendMessage', message)
+})
--- src/index.js
+++ src/index.js
@@ -12,8 +12,16 @@ const publicDirectoryPath = path.join(__dirname,'../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+// const message = "Welcome!"
+
+io.on('connection', (socket) => {
     console.log("New Websocket Connection is made")
+
+    socket.emit('message', 'Welcome!')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
 })
 server.listen(port, () => {
     console.log('Express server is running on ' + port +'!')
